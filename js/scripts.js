98ac63acf087e488481b02bb4000d5568dfe3577
leastburg <leastburg@gmail.com>
2022-04-13 13:47:29 -0600

    add script to take checkbox input and return associated ingredients in console, WIP ingredient library

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,41 @@
 //This is business logic.//
 
+const ingredientsVeggieSpringRolls = ["rice paper", "rice noodles", "cucumber", "carrot", "mixed greens", "avocado", "hoisin sauce", "peanut sauce"];
+const ingredientsPickledBeetSalad =  ["mixed greens", "pickled beets", "goat cheese", "sundried tomatoes", "red onion", "avocado", "olive oil", "balsamic vinegar", "cucumber"];
+
+function displayIngredients(list) {
+  let combinedIngredientsList = [];
+  list.forEach(function(recipe) {
+    if (recipe === "spring-rolls") {
+      combinedIngredientsList = combinedIngredientsList.concat(ingredientsVeggieSpringRolls);
+    } else if (recipe === "salad") {
+      combinedIngredientsList = combinedIngredientsList.concat(ingredientsPickledBeetSalad);
+    }
+    return combinedIngredientsList;
+  });
+  console.log(combinedIngredientsList);
+}
+
 
 //This is user interface logic.//
 $(document).ready(function() {
+
+  let recipeListArray = [];
+  
   $("form#select-recipes").submit(function(event) {
     event.preventDefault();
     $("#grocery-list").show();
     $("input:checkbox[name=recipe-options]:checked").each(function() {
-      const groceryList = $(this).val();
-      $('#grocery-list').append(groceryList + "<br>");
+      recipeListArray.push($(this).val());
+
+      // const groceryList = $(this).val();
+      // console.log(groceryList);
+
+      // displayIngredients(groceryList);
+      // $('#grocery-list').append(groceryList + "<br>");
     });
+    console.log(recipeListArray);
+    displayIngredients(recipeListArray);
     $('#select-recipes').hide();
   });
 
